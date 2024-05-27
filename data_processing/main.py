93ff0a625fb0190ac45ce5bc9fc46a856451ff18
annabsalin <salinannabelle@gmail.com>
2024-05-27 11:51:33 +0200

    Pour Noé
    update

--- data_processing/main.py
+++ data_processing/main.py
@@ -32,10 +32,10 @@ def symmetric_index(image):
 
 # Feature 4 - The min, max and mean values for Red, Green and Blue within the bug mask
 
-def extract_color_stats(image_folder, mask_folder):
+def extract_color_stats(image_dir, mask_dir):
     # Lire l'image et le masque
-    image = cv2.imread(image_folder)
-    mask = cv2.imread(mask_folder, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(image_dir)
+    mask = cv2.imread(mask_dir, cv2.IMREAD_GRAYSCALE)
     
     # Appliquer le masque pour extraire les pixels de l'insect
     bug_pixels = cv2.bitwise_and(image, image, mask=mask)
@@ -73,3 +73,45 @@ def extract_color_stats(image_folder, mask_folder):
 
 
 # Feature 5 - The median and standard deviation for the Red, Green and Blue within the bug mask
+def median_std(image, mask):
+    bee_isolation = cv2.bitwise_and(image, image, mask=mask)
+    bee_pixels = bee_isolation[mask != 0]
+    bee_pixels = bee_pixels.reshape(-1, 3)
+    median_values = np.median(bee_pixels, axis=0)
+    std_values = np.std(bee_pixels, axis=0)
+    result = np.concatenate((median_values, std_values))
+    if len(result) != 6:
+        return [None]*6
+    return result.tolist()
+
+
+#Process directory 
+def process_directory(images_dir, masks_dir, output_file):
+    results = []
+    for image_filename in os.listdir(images_dir):
+        if image_filename.endswith('.jpg'):  # Ensure this matches your image file extensions
+            image_path = os.path.join(images_dir, image_filename)
+            mask_filename = image_filename.replace('.jpg', '_mask.png')  # Adjust mask file pattern as necessary
+            mask_path = os.path.join(masks_dir, mask_filename)
+            
+            image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+            mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+            
+            if image is not None and mask is not None:
+                stats = median_std(image, mask)
+                results.append([image_filename] + stats)
+            else:
+                print(f"Failed to load image or mask for {image_filename}")
+
+    # Convert results to a DataFrame and save as CSV
+    df = pd.DataFrame(results, columns=['Image', 'Median Red', 'Median Green', 'Median Blue', 'Std Dev Red', 'Std Dev Green', 'Std Dev Blue'])
+    df.to_csv(output_file, index=False)
+    print(f"Results saved to {output_file}")
+
+# Define your directories and output file
+images_dir = 'train\images_1_to_250'
+masks_dir = 'train\masks'
+output_file = 'train\classif.xlsx'
+
+# Process the images
+process_directory(images_dir, masks_dir, output_file)
